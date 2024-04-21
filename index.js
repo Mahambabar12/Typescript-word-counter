bac61ee066d487de318c06ba38e0ffcd2096f6f6
Mahambabar12 <mahambabar11@gmail.com>
2024-04-20 22:18:49 -0700

    word counter

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+const answers = await inquirer.prompt([
+    {
+        name: 'Sentence',
+        type: 'input',
+        message: chalk.bold.magentaBright('Enter your sentence to count the word:')
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.bold.italic.bgMagenta(`*************** your sentence words counting is ${words.length} ***************`));
